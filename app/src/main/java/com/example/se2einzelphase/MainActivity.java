f9b07d07f2eccfcf23c6916fb8d3f72463be6eb7
Philipp <philipprauscher26@gmail.com>
2022-03-04 10:41:52 +0100

    Created Activity

--- app/src/main/java/com/example/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/example/se2einzelphase/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.se2einzelphase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText matrikelnummer;
+    private EditText output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        matrikelnummer = findViewById(R.id.matrikelnummerInput);
+        output = findViewById(R.id.editTextAnswerServer);
+    }
+
+    public void buttonSend_Click(View view)
+    {
+        String input = matrikelnummer.getText().toString();
     }
 }
